b3eb3dec16250022f89d8bca5abbabef734ca3e9
Aman Kumar Lall <55016543+amanlall@users.noreply.github.com>
2025-07-11 02:36:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/noteapp/', // <-- this is the fix (use your actual repo name here)
+    base: '/noteapp/', // <-- this is the fix (use your actual repo name here) deploy
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
